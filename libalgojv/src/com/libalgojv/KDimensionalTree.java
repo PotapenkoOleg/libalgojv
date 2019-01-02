e8e3b82f790ba9d71853de01f0dc75d000a0626c
PotapenkoOleg <PotapenkoOleg@gmail.com>
2019-01-02 12:56:42 -0500

    K-Dimentional Tree implementation file

---
+++ libalgojv/src/com/libalgojv/KDimensionalTree.java
@@ -0,0 +1,12 @@
+/*
+ * KDTree.java
+ * Project libalgoj
+ *
+ * Created by Oleg Potapenko on 11/29/18 11:23 PM.
+ * Copyright © 2018-2018 Oleg Potapenko. All rights reserved.
+ */
+
+package com.libalgojv.trees.KDimensionalTree;
+
+public class KDimensionalTree implements BinarySearchTree {
+}
