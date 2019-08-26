c5e74f21d368fbde3c5a81d030ec8ba3afd4799c
PotapenkoOleg <PotapenkoOleg@gmail.com>
2019-08-26 12:13:17 -0400

    Copyright update

--- libalgojv/src/com/libalgojv/tries/TernaryTrie.java
+++ libalgojv/src/com/libalgojv/tries/TernaryTrie.java
@@ -1,3 +1,11 @@
+/*
+ * TernaryTrie.java
+ * Project libalgojv
+ *
+ * Created by Oleg Potapenko on 12/25/18 12:00 PM.
+ * Copyright © 2018-2019 Oleg Potapenko. All rights reserved.
+ */
+
 package com.libalgojv.tries;
 
 import com.libalgojv.common.interfaces.Queue;
